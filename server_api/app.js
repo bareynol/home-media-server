ed88ee37a1e44bb905ea10b0a1753f8aeca38114
Brian Reynolds <brian@growratio.com>
2020-03-10 12:37:25 -0400

    CPU temperature outputting

--- server_api/app.js
+++ server_api/app.js
@@ -6,27 +6,30 @@ const port = 3000
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/cpu', (req, res) => {
-  console.log("cpu temp:", getCpuTemperature());
-  res.send('CPU')
+app.get('/cpu', async (req, res) => {
+  const cpuTemp = await getCpuTemperature();
+  console.log("cpu temp:", cpuTemp);
+  res.send(`CPU Temp: ${cpuTemp}`)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 async function getCpuTemperature() {
-  return exec('sensors', function(error, stdout) {
-    if (error) {
-      return ('ERROR READING CPU');
-    } else {
-      let lines = stdout.toString().split('\n');
-      lines.forEach(function (line) {
-        let regex = /[+-]([^°]*)/g;
-        let temps = line.match(regex);
-        let firstPart = line.split(':')[0].toUpperCase();
-        if (firstPart.indexOf('TDIE') !== -1) {
-          return parseFloat(temps[0])
-        }
-      });
-    }
+  return new Promise((resolve, reject) => {
+    exec('sensors', function(error, stdout) {
+      if (error) {
+        return resolve('ERROR READING CPU');
+      } else {
+        let lines = stdout.toString().split('\n');
+        lines.forEach(function (line) {
+          let regex = /[+-]([^°]*)/g;
+          let temps = line.match(regex);
+          let firstPart = line.split(':')[0].toUpperCase();
+          if (firstPart.indexOf('TDIE') !== -1) {
+            return resolve(parseFloat(temps[0]))
+          }
+        });
+      }
+    })
   })
 }
