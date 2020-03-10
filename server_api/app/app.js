36ce223db96093fb57d2cec371c6fb42ef8e5a35
Brian Reynolds <brireyn@gmail.com>
2020-03-10 16:36:04 -0400

    added route to send json data

--- server_api/app/app.js
+++ server_api/app/app.js
@@ -1,35 +1,12 @@
 const express = require('express');
-const si = require('systeminformation');
-const exec = require('child_process').exec;
+const {getAllData} = require('./utils');
+
 const app = express()
 const port = 3000
 
-
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/cpu', async (req, res) => {
-  const cpuTemp = await getCpuTemperature();
-  console.log("cpu temp:", cpuTemp);
-  res.send(`CPU Temp: ${cpuTemp}`)
+app.get('/stats', async (req, res) => {
+  const stats = await getAllData();
+  res.json(stats);
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-async function getCpuTemperature() {
-  return new Promise((resolve, reject) => {
-    exec('sensors', function(error, stdout) {
-      if (error) {
-        return resolve('ERROR READING CPU');
-      } else {
-        let lines = stdout.toString().split('\n');
-        lines.forEach(function (line) {
-          let regex = /[+-]([^°]*)/g;
-          let temps = line.match(regex);
-          let firstPart = line.split(':')[0].toUpperCase();
-          if (firstPart.indexOf('TDIE') !== -1) {
-            return resolve(parseFloat(temps[0]))
-          }
-        });
-      }
-    })
-  })
-}
+app.listen(port);
